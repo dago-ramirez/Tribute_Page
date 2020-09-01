98781f82e5ec208a6b2fafb5f1d98c9b44fc9987
Dago R <dagoramirezserna@gmail.com>
2020-09-01 00:04:32 -0500

    Se agrega bootstrap al proyecto

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log(screen.width, window.innerWidth);
+let smallScreen = document.querySelector(".small-screen");
+if (window.innerWidth < 576) {
+    // alert("pantalla pequeña");
+    // smallScreen.classList.add("small");
+}
